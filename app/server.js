edf4a5e02693a9c55a67166fec8b29a515ae889b
raulespecialist <meloadik@gmail.com>
2021-08-24 18:07:21 -0500

    final

--- app/server.js
+++ app/server.js
@@ -1,22 +1,47 @@
-var express = require('express') //llamamos a Express
-var app = express()               
+const fetch = require('node-fetch') //llamamos a node fetch
+const express = require('express') //llamamos a Express
+const app = express()               
+const api = 'https://api.mercadolibre.com';
 
-var port = process.env.PORT || 8080  // establecemos nuestro puerto
+const port = process.env.PORT || 8080  // establecemos nuestro puerto
 
 app.get('/', function(req, res) {
-  res.json({ mensaje: '¡Hola Mundo!' })   
-})
-
-app.get('/cervezas', function(req, res) {
-  res.json({ mensaje: '¡A beber cerveza!' })  
+  //res.json({ mensaje: '¡Hola Mundo!' })
+  fetch('https://api.mercadolibre.com/sites/MLM/search?category=MLM1055&sort=price_asc&search_type=scan')
+  .then((respuesta) => {
+    return respuesta.json()
+  }).then((resp) => {
+    //console.log(resp)
+    articulos = [];
+    resp.results.forEach(obji);
+    function obji(item) {
+      let sellerid = item.seller.id;
+      var response = fetch(api + '/users/' + sellerid);
+      sellername = response;
+      let marca = item.attributes[0].value_name;
+      let envio = item.shipping.free_shipping;
+      let logistica = item.shipping.logistic_type;
+      let loperacion = item.address.state_name + item.address.city_name;
+      let condicion = item.attributes[1].value_name;
+      let rprecios = item.price;
+      var respuestajson = {
+        SellerID: sellerid,
+        SellerName: sellername,
+        Marca: marca,
+        EnvioGratis: envio,
+        TipoDeLogistica: logistica,
+        LugarOperacionSeller: loperacion,
+        CondicionArticulo: condicion,
+        RangoPrecios: rprecios,
+      }
+      articulos.push(respuestajson);
+    }
+    res.json(articulos);
+  })
 })
 
 app.post('/', function(req, res) {
   res.json({ mensaje: 'Método post' })   
-})
-
-app.del('/', function(req, res) {
-  res.json({ mensaje: 'Método delete' })  
 })
 
 // iniciamos nuestro servidor
